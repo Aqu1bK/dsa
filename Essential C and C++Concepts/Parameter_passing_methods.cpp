aa43fe34a1f539e5aaf7ef88b5e11f0c7f2a3e54
Aqu1bK <aquibkhan7221@gmail.com>
2024-12-09 19:46:27 +0530

    Array as parameter

---
+++ Essential C and C++Concepts/Parameter_passing_methods.cpp
@@ -0,0 +1,74 @@
+/*---------Passing By Value----------*/
+
+// #include <iostream>
+
+// using namespace std;
+
+// void swap(int x, int y)
+// {
+//     int temp;
+//     temp = x;
+//     x=y;
+//     y-temp;
+// }
+
+// int main() 
+// {
+//     int num1 = 10, num2 = 15;
+//     swap(num1,num2);
+
+//     cout<<"First Number "<<num1<<endl;
+//     cout<<"Second Number "<<num2<<endl;
+
+//     return 0;
+// }
+
+/*---------Passing By Address----------*/
+
+// #include <iostream>
+
+// using namespace std;
+
+// void swap(int *x, int *y)
+// {
+//     int temp;
+//     temp = *x;
+//     *x=*y;
+//     *y=temp;
+// }
+
+// int main() 
+// {
+//     int num1 = 10, num2 = 15;
+//     swap(&num1,&num2);
+
+//     cout<<"First Number "<<num1<<endl;
+//     cout<<"Second Number "<<num2<<endl;
+
+//     return 0;
+// }
+
+/*---------Passing By References----------*/
+
+#include <iostream>
+
+using namespace std;
+
+void swap(int &x, int &y)
+{
+    int temp;
+    temp = x;
+    x=y;
+    y=temp;
+}
+
+int main() 
+{
+    int num1 = 10, num2 = 15;
+    swap(num1,num2);
+
+    cout<<"First Number "<<num1<<endl;
+    cout<<"Second Number "<<num2<<endl;
+
+    return 0;
+}
